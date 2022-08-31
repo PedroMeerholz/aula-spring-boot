35e0d2ab1678679d8fe4b313a6f596aee5ddc8f1
Pedro <developer.pedromeerholz@gmail.com>
2022-08-30 21:13:48 -0300

    Criação do arquivo principal do sistema

---
+++ controle-frota/src/main/java/io/github/pedromeerholz/controlefrota/ControleFrotaApplication.java
@@ -0,0 +1,14 @@
+package io.github.pedromeerholz.controlefrota;
+
+import io.github.pedromeerholz.controlefrota.model.Funcionario;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleFrotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleFrotaApplication.class, args);
+	}
+
+}
