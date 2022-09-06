499c1cab968b8975034b64238463b64383830f85
Pedro <developer.pedromeerholz@gmail.com>
2022-09-06 10:41:07 -0300

    Criação do model Veiculo

---
+++ controle-frota/src/main/java/io/github/pedromeerholz/controlefrota/model/Veiculo.java
@@ -0,0 +1,47 @@
+package io.github.pedromeerholz.controlefrota.model;
+
+public class Veiculo {
+    private String placa;
+    private String modelo;
+    private String ano;
+    private boolean status;
+
+    public Veiculo(String placa, String modelo, String ano, boolean status) {
+        setPlaca(placa);
+        setModelo(modelo);
+        setAno(ano);
+        setStatus(status);
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
