abf77f141caadc2e046156660642207e04f3507e
Pedro <developer.pedromeerholz@gmail.com>
2022-09-06 09:59:22 -0300

    Upload dos diretórios resources e test

---
+++ controle-frota/src/test/java/io/github/pedromeerholz/controlefrota/ControleFrotaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pedromeerholz.controlefrota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFrotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
